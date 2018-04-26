a40670491e46a6d9dc73d245c86c0d333272711d
Diogo Branquinho Ramos <diogobranquinho@tecsus.com.br>
2018-04-25 21:52:01 -0300

    Incluindo a versão para Python 2.7

---
+++ 2.7/thread/client_tcp.py
@@ -0,0 +1,16 @@
+#Cliente TCP
+import socket
+# Endereco IP do Servidor
+SERVER = '127.0.0.1'
+# Porta que o Servidor esta escutando
+PORT = 5002
+tcp = socket.socket(socket.AF_INET,
+socket.SOCK_STREAM)
+dest = (SERVER, PORT)
+tcp.connect(dest)
+print ('Para sair use CTRL+X\n')
+msg = raw_input()
+while msg <> '\x18':
+    tcp.send (msg)
+    msg = raw_input()
+tcp.close()
---
+++ 2.7/thread/server_thread_tcp.py
@@ -0,0 +1,25 @@
+#Servidor TCP
+import socket
+from threading import Thread
+
+def conexao(con,cli):
+    while True:
+        msg = con.recv(1024)
+        if not msg: break
+        print msg
+    print 'Finalizando conexao do cliente', cli
+    con.close() 
+
+# Endereco IP do Servidor
+HOST = ''
+# Porta que o Servidor vai escutar
+PORT = 5002
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+orig = (HOST, PORT)
+tcp.bind(orig)
+tcp.listen(1)
+while True:
+    con, cliente = tcp.accept()
+    print 'Concetado por ', cliente
+    t = Thread(target=conexao, args=(con,cliente,))
+    t.start()
